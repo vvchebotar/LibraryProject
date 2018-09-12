1fa8426656d36533b2482e496893d0688f1ad216
vvchebotar <v.v.chebotar@yandex.ru>
2018-09-13 01:56:14 +0300

    renamed, corrected and refactored beans

--- src/org/chebotar/libraryapp/beans/Book.java
+++ src/org/chebotar/libraryapp/beans/Book.java
@@ -3,85 +3,72 @@ package org.chebotar.libraryapp.beans;
 import java.time.LocalDate;
 
 public class Book {
-	 	   
-	private String id;
-	private String name;
-	private String author;
-	private int publicationDate;
-	private LocalDate dateOfReceipt;
-	private String shortDescription;
-	private int numberOfPages;
 
-	public Book() {	 
-	}
-	public Book(String id, String name, String author, int publicationDate, LocalDate dateOfReceipt, String shortDescription, int numberOfPages) {
-		this.id = id;
-		this.setName(name);
-		this.setAuthor(author);
-		this.setPublicationDate(publicationDate);
-		this.setDateOfReceipt(dateOfReceipt);
-		this.setShortDescription(shortDescription);
-		this.setNumberOfPages(numberOfPages);	       
-	}
-	   
-	public String getId() {
-		return id;
-	}
+    private String id;
+    private String name;
+    private String author;
+    private int publicationDate;
+    private LocalDate dateOfReceipt;
+    private String shortDescription;
+    private int numberOfPages;
 
-	public void setId(String id) {
-		this.id = id;
-	}
+    public String getId() {
+        return id;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public void setId(String id) {
+        this.id = id;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public String getAuthor() {
-		return author;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public void setAuthor(String author) {
-		this.author = author;
-	}
+    public String getAuthor() {
+        return author;
+    }
 
-	public int getPublicationDate() {
-		return publicationDate;
-	}
+    public void setAuthor(String author) {
+        this.author = author;
+    }
 
-	public void setPublicationDate(int publicationDate) {
-		this.publicationDate = publicationDate;
-	}
+    public int getPublicationDate() {
+        return publicationDate;
+    }
 
-	public LocalDate getDateOfReceipt() {
-		return dateOfReceipt;
-	}
+    public void setPublicationDate(int publicationDate) {
+        this.publicationDate = publicationDate;
+    }
 
-	public void setDateOfReceipt(LocalDate dateOfReceipt) {		
-		this.dateOfReceipt = dateOfReceipt;		
-	}
-	
-	public void setDateOfReceipt(long dateOfReceipt) {		
-		this.dateOfReceipt = LocalDate.ofEpochDay(dateOfReceipt);		
-	}
+    public LocalDate getDateOfReceipt() {
+        return dateOfReceipt;
+    }
 
+    public void setDateOfReceipt(LocalDate dateOfReceipt) {
+        this.dateOfReceipt = dateOfReceipt;
+    }
 
-	public String getShortDescription() {
-		return shortDescription;
-	}
+    public void setDateOfReceipt(long dateOfReceipt) {
+        this.dateOfReceipt = LocalDate.ofEpochDay(dateOfReceipt);
+    }
 
-	public void setShortDescription(String shortDescription) {
-		this.shortDescription = shortDescription;
-	}
+    public String getShortDescription() {
+        return shortDescription;
+    }
 
-	public int getNumberOfPages() {
-		return numberOfPages;
-	}
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
 
-	public void setNumberOfPages(int numberOfPages) {
-		this.numberOfPages = numberOfPages;
-	}	 
+    public int getNumberOfPages() {
+        return numberOfPages;
+    }
+
+    public void setNumberOfPages(int numberOfPages) {
+        this.numberOfPages = numberOfPages;
+    }
 }
